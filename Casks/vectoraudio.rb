e1dc11f31c5c4420df4f4856e18d2e812b97010b
Marc <info@marcsauer.net>
2023-07-24 21:22:08 +0200

    Version 1.3.1

--- Casks/vectoraudio.rb
+++ Casks/vectoraudio.rb
@@ -1,8 +1,8 @@
 cask "vectoraudio" do
-  version "1.2.1"
-  sha256 "2f38f359e10cff44549675e6ba8cbf7d2bf0ed7d0c62741c28139c23bc6512e2"
+  version "1.3.1"
+  sha256 "628bccd1ab90f7ad7d02f7ee104a1111c987ce32546a6b5652c23fe9736cdf58"
 
-  url "https://github.com/pierr3/VectorAudio/releases/download/v1.2.1/VectorAudio-v1.2.1-Installer.dmg"
+  url "https://github.com/pierr3/VectorAudio/releases/download/v1.3.1/VectorAudio-v1.3.1-Universal.dmg"
   name "VectorAudio"
   desc "An Audio-For-VATSIM ATC Client for macOs and Linux"
   homepage "https://github.com/pierr3/VectorAudio"
